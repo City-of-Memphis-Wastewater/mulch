0d918a60bb36244635dfb2384892f5ae1bda69fd
George Clayton Bennett <GEORGE.BENNETT@memphistn.gov>
2025-08-08 13:18:45 -0500

    installation script improvement

--- scripts/install/mulch-workspace.ps1
+++ scripts/install/mulch-workspace.ps1
@@ -9,13 +9,10 @@ if (-Not (Test-Path -Path $path)) {
 
 Set-Location -Path $path
 
-# Where does mulch.exe live on a Windows system
+## Where does mulch.exe live on a Windows system
 $env:PATH += ";$env:USERPROFILE\.local\bin"
 
-# make this path if it does not exsit, and inject the mulch-icon.ico file and the mulch-workspace.ps1 file
-#";$env:USERPROFILE\.mulch"
-
-# Run mulch init
-mulch workspace --here #--pattern new --here # --pattern date
+## Run mulch init
+mulch workspace --here --pattern new # --pattern date
 
 # Start-Sleep -Seconds 1
